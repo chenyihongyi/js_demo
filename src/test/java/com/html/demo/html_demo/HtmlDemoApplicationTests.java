689b9ee85b73d08403c945d304c74487cbebbc53
Your Name <chenyihongyi@163.com>
2020-02-29 05:59:06 +0800

    新建js项目

---
+++ src/test/java/com/html/demo/html_demo/HtmlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.html.demo.html_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
